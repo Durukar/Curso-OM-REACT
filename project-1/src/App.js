1d58553d9df2249ff3d333a05c6d699043f3f7f8
Lucas Davila <nightdavila@gmail.com>
2023-06-08 09:49:21 -0300

    Aula 16 (15 era informativo). State com arrays e objetos | inicio

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -7,64 +7,13 @@ class App extends Component {
       job: 'Lucas Davila'
     };
 
-  handlePClick = () => {
-    this.setState({ name: 'Lucas Júnior' })
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { job } = this.state;
-    if (job === 'Desenvolvedor Junior Java + React') {
-      this.setState({ job: 'Lucas Davila' })
-    } else {
-      this.setState({ job: 'Desenvolvedor Junior Java + React' })
-    }
-  }
-
   render () {
     const { job }  = this.state;
     return (
           <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                {job}
-              </p>
-              <a
-                onClick={this.handleAClick}
-                className="App-link"
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-                title='Alterar nome'
-              >
-                Clique e descubra
-              </a>
-            </header>
           </div>
         );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
