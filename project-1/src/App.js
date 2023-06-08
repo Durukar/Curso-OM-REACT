7638041cfe2f3e9f35bc9056696f826a91a3c9bb
Lucas Davila <nightdavila@gmail.com>
2023-06-08 10:02:19 -0300

    Aula 16. State com Arrays e objetos | Fim

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,18 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   state = {
-      job: 'Lucas Davila'
-    };
+    posts: [
+      {
+        id: 1,
+        title: 'Titulo 1',
+        body: 'Corpo 1'
+      },
+      {
+        id: 2,
+        title: 'Titulo 2',
+        body: 'Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'Titulo 3',
+        body: 'Corpo 3'
+      },
+    ]
+  };
 
-  render () {
-    const { job }  = this.state;
+  render() {
+    const { posts } = this.state;
     return (
-          <div className="App">
+      <div className="App">
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
           </div>
-        );
+        ))}
+      </div>
+    );
   }
 }
 
